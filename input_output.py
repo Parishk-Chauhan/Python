9dcd5acfbfd759fc068be8f2c79d1821342db0c5
Parishk Chauhan <parishkchauhan@gmail.com>
2024-09-16 10:29:04 +0530

    input/output

---
+++ input_output.py
@@ -0,0 +1,28 @@
+# using print function to show output 
+
+
+print("hello")
+
+# taking input in python using input function 
+
+name = input("Enter your name : ")
+
+print(name)            # showing the name entered by user 
+
+# if i want my name to be entered in next line : 
+
+name = input("Enter your name : \n")
+
+
+# taking multiple inputs : using split()
+
+name, age = input("Enter your age name and age : ").split()
+
+print(name)
+print(age)
+
+# what is split() ?
+
+# The string manipulation function in Python used to break down a bigger string into several smaller strings is called the split() function in Python. The split() function returns the strings as a list.  
+
+# more about split in string manipulation functions : 
